9ff11996b8d1ffd22a988b067fe0729a3aadd4d3
rachd <rachel.m.dorn@gmail.com>
2020-09-12 11:15:11 -0400

    populate hero data

--- HeroSelectCard.gd
+++ HeroSelectCard.gd
@@ -3,14 +3,22 @@ extends VBoxContainer
 signal hero_selected(heroId)
 signal hero_deselected(heroId)
 
-var heroId = 1
+export var heroId: int
 
 func _on_CheckButton_toggled(button_pressed):
 	if button_pressed == true:
 		emit_signal("hero_selected", heroId)
 	else:
 		emit_signal("hero_deselected", heroId)
+		
+func _populate_data():
+	var hero_stats = constants.get_hero_stats()[heroId]
+	$Name.text = "Name " + str(hero_stats["name"])
+	$Speed.text = "Speed " + str(hero_stats["speed"])
+	$Health.text = "Health " + str(hero_stats["health"])
+	$Attack.text = "Attack " + str(hero_stats["attack"])
 
 func _ready():
+	_populate_data()
 	self.connect("hero_selected", get_node("/root/HeroSelection"), "_on_Hero_seleted")
 	self.connect("hero_deselected", get_node("/root/HeroSelection"), "_on_Hero_deseleted")
---
+++ constants.gd
@@ -0,0 +1,29 @@
+extends Node
+
+static func get_hero_stats():
+	return {
+		1: {
+			"name": "Zeus",
+			"health": 100,
+			"speed": 20,
+			"attack": 60
+		},
+		2: {
+			"name": "Hermes",
+			"health": 70,
+			"speed": 50,
+			"attack": 40
+		},
+		3: {
+			"name": "Artemis",
+			"health": 80,
+			"speed": 40,
+			"attack": 50
+		},
+		4: {
+			"name": "Poseidon",
+			"health": 90,
+			"speed": 30,
+			"attack": 50
+		}
+	}
