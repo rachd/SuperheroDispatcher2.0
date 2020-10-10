747461bfca1554276c28f3fafd7fd76f3d763f34
rachd <rachel.m.dorn@gmail.com>
2020-10-10 12:48:56 -0400

    move upequipped to separate checkbox

--- MyItems/FilterDropdown.gd
+++ MyItems/FilterDropdown.gd
@@ -2,19 +2,16 @@ extends OptionButton
 
 signal show_all_items
 signal filter_by_type(itemType)
-signal show_unequipped
 
 func _ready():
 	self.connect("show_all_items", get_node("/root/MyItems/Content/ItemList"), "_show_all_items")
 	self.connect("filter_by_type", get_node("/root/MyItems/Content/ItemList"), "_filter_by_type")
-	self.connect("show_unequipped", get_node("/root/MyItems/Content/ItemList"), "_show_unequipped_items")
 	add_item("Filter", 0)
 	add_item("Head", 1)
 	add_item("Left Hand", 2)
 	add_item("Right Hand", 3)
 	add_item("Body", 4)
 	add_item("Foot", 5)
-	add_item("Unequipped", 6)
 
 
 func _on_FilterDropdown_item_selected(index):
@@ -31,5 +28,3 @@ func _on_FilterDropdown_item_selected(index):
 			emit_signal("filter_by_type", "body")
 		5:
 			emit_signal("filter_by_type", "foot")
-		6:
-			emit_signal("show_unequipped")
--- MyItems/ItemList.gd
+++ MyItems/ItemList.gd
@@ -2,6 +2,8 @@ extends VBoxContainer
 
 var item_row_scene = preload("res://MyItems/ItemRow.tscn")
 var item_rows = {}
+var current_filter = null
+var show_equipped = true
 
 func _render_items():
 	for owned_item_id in gameVariables.owned_item_ids:
@@ -36,6 +38,8 @@ func on_hero_tab_selected(heroId):
 func equip_item(itemId, heroId):
 	item_rows[itemId].set_equipped()
 	item_rows[itemId].set_assigned_hero(heroId)
+	if not show_equipped:
+		item_rows[itemId].hide()
 
 func unequip_item(itemId):
 	item_rows[itemId].set_unequipped()
@@ -46,15 +50,34 @@ func remove_item(itemId):
 	item_rows.erase(itemId)
 	
 func _show_all_items():
+	current_filter = null
 	for item_row in item_rows.values():
 		item_row.show()
+	if !show_equipped:
+		_hide_equipped()
 		
 func _filter_by_type(itemType):
+	current_filter = itemType
 	for item_row in item_rows.values():
 		item_row.hide_if_type(itemType)
+	if !show_equipped:
+		_hide_equipped()
+	
+			
+func _on_items_sorted():
+	for item_row in item_rows.values():
+		item_row.queue_free()
+	_render_items()
 
-func _show_unequipped_items():
-	_show_all_items()
+
+func _on_UnequppedCheckBox_toggled(button_pressed):
+	if button_pressed:
+		_hide_equipped()
+	else:
+		_show_equipped()
+	
+func _hide_equipped():
+	show_equipped = false
 	var assigned_items = []
 	for hero in gameVariables.assigned_items.keys():
 		assigned_items += gameVariables.assigned_items[hero].values()
@@ -62,8 +85,10 @@ func _show_unequipped_items():
 		if item_id in assigned_items:
 			item_rows[item_id].hide()
 			
-func _on_items_sorted():
-	for item_row in item_rows.values():
-		item_row.queue_free()
-	_render_items()
+func _show_equipped():
+	show_equipped = true
+	if current_filter == null:
+		_show_all_items()
+	else:
+		_filter_by_type(current_filter)
 			
